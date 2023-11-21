36ca442e1e26f29fc2a752d78b9c34c51298daeb
localdvloper <54663383+localdvloper@users.noreply.github.com>
2023-11-21 22:13:50 +0900

    Update Package.swift
    번들 추가, 폰트 추가

--- Package.swift
+++ Package.swift
@@ -26,9 +26,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GreenPFramework",
-            dependencies: ["Kingfisher", "Alamofire", "SnapKit"]),
+            dependencies: ["Kingfisher", "Alamofire", "SnapKit"],
+            resources: [.process("Fonts"), Resource.process("Assets.xcassets")]),
         .testTarget(
             name: "GreenPFrameworkTests",
-            dependencies: ["GreenPFramework"]),
+            dependencies: ["GreenPFramework"])
     ]
 )
